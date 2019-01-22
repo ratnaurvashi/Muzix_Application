dd5d12606f9d80eb67e67093ad0477956e47fe98
urvashi.ratna <urvashi.ratna@boeing.com>
2019-01-22 17:36:05 +0530

    added Swagger2

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,45 +1,22 @@
 package com.stackroute.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track {
     @Id
     private int trackId;
     private String trackName;
     private String comments;
 
-    public Track() {
-    }
-
-    public Track(int trackId, String trackName, String comments) {
-        this.trackId = trackId;
-        this.trackName = trackName;
-        this.comments = comments;
-    }
-
-    public int getTrackId() {
-        return trackId;
-    }
-
-    public void setTrackId(int trackId) {
-        this.trackId = trackId;
-    }
-
-    public String getTrackName() {
-        return trackName;
-    }
-
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
 }
