14bbf12d0ca858175be53af31cf224d40b529540
urvashi.ratna <urvashi.ratna@boeing.com>
2019-01-22 22:53:29 +0530

    added startup logic

---
+++ src/main/java/com/stackroute/ApplicationStartup.java
@@ -0,0 +1,19 @@
+package com.stackroute;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
+
+    /**
+     * This event is executed as late as conceivably possible to indicate that
+     * the application is ready to service requests.
+     */
+    @Override
+    public void onApplicationEvent(final ApplicationReadyEvent event) {
+        System.out.println("Welcome to Spring Boot Application");
+    }
+
+}
